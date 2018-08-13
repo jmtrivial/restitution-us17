ec5b0ede7801d6090375ec33f79dee87f13862ba
Jean-Marie Favreau <jeanmarie.favreau@free.fr>
2018-08-13 14:40:49 +0000

    Move the python version into its own directory

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ if [ ! `screen -list|grep maquette|wc -l` -eq 0 ]; then
 fi
 
 echo "START AUDIO"
-echo "screen -S maquette -d -m $DIR/maquette.py"
+echo "screen -S maquette -d -m $DIR/python/maquette.py"
 screen -S "maquette" -d -m $DIR/maquette.py
 
 
